98d5197fccf7cada76d2ad6d2cb8bee8839e6dc6
Curt Binder <curt.binder@gmail.com>
2011-10-20 09:25:18 -0400

    Initial Commit

---
+++ src/info/curtbinder/reefangel/phone/ReefAngelStatusActivity.java
@@ -0,0 +1,13 @@
+package info.curtbinder.reefangel.phone;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ReefAngelStatusActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
