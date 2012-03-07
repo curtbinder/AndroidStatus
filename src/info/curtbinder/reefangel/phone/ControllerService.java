826e567f69d3906cb82ca2bc2ff56c578ec6678d
Curt Binder <curt.binder@gmail.com>
2012-03-06 22:43:28 -0500

    Added copyright notice to all code and xml files

--- src/info/curtbinder/reefangel/phone/ControllerService.java
+++ src/info/curtbinder/reefangel/phone/ControllerService.java
@@ -1,5 +1,13 @@
 package info.curtbinder.reefangel.phone;
 
+/*
+ * Copyright (c) 2011-12 by Curt Binder (http://curtbinder.info)
+ *
+ * This work is made available under the terms of the 
+ * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License
+ * http://creativecommons.org/licenses/by-nc-sa/3.0/
+ */
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
--- src/info/curtbinder/reefangel/phone/Relay.java
+++ src/info/curtbinder/reefangel/phone/Relay.java
@@ -1,5 +1,13 @@
 package info.curtbinder.reefangel.phone;
 
+/*
+ * Copyright (c) 2011-12 by Curt Binder (http://curtbinder.info)
+ *
+ * This work is made available under the terms of the 
+ * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License
+ * http://creativecommons.org/licenses/by-nc-sa/3.0/
+ */
+
 public class Relay {
 	static final byte PORT_OFF = 0;
 	static final byte PORT_ON = 1;
