b5c342bb6519349fac27afbcef38b730655d1ffc
Curt Binder <curt.binder@gmail.com>
2014-03-23 21:59:27 -0400

    Updated notifications fragment to display list
    Updated list layout
    Added code to retrieve notifications from database
    Still have to add in the Add functionality

---
+++ AndroidStatus/src/main/java/info/curtbinder/reefangel/phone/NotificationListCursorAdapter.java
@@ -0,0 +1,73 @@
+package info.curtbinder.reefangel.phone;
+
+/*
+ * Copyright (c) 2011-2013 by Curt Binder (http://curtbinder.info)
+ *
+ * This work is made available under the terms of the Creative Commons
+ * Attribution-NonCommercial-ShareAlike 3.0 Unported License
+ * http://creativecommons.org/licenses/by-nc-sa/3.0/
+ */
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.support.v4.widget.CursorAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+import info.curtbinder.reefangel.db.NotificationTable;
+
+/**
+ * Created by binder on 3/23/14.
+ */
+
+public class NotificationListCursorAdapter extends CursorAdapter {
+
+    static private String[] deviceParameters;
+    static private String[] notifyConditions;
+    private final int LAYOUT = R.layout.frag_notification_item;
+
+    public NotificationListCursorAdapter(Context context, Cursor c, int flags) {
+        super(context, c, flags);
+        final Resources r = context.getResources();
+        deviceParameters = r.getStringArray(R.array.deviceParameters);
+        notifyConditions = r.getStringArray(R.array.notifyConditions);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        final ViewHolder vh = (ViewHolder) view.getTag();
+        setViews(vh, cursor);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        final LayoutInflater inflater = LayoutInflater.from(context);
+        View v = inflater.inflate(LAYOUT, parent, false);
+        ViewHolder vh = new ViewHolder();
+        vh.item = (TextView) v.findViewById(R.id.notification_item);
+        setViews(vh, cursor);
+        v.setTag(vh);
+        return v;
+    }
+
+    private void setViews(ViewHolder v, Cursor c) {
+        int param = c.getInt(c.getColumnIndex(NotificationTable.COL_PARAM));
+        int cond = c.getInt(c.getColumnIndex(NotificationTable.COL_CONDITION));
+
+        String s = String.format(Locale.US,
+                "%s %s %s",
+                deviceParameters[param],
+                notifyConditions[cond],
+                c.getString(c.getColumnIndex(NotificationTable.COL_VALUE)));
+        v.item.setText(s);
+    }
+
+    static class ViewHolder {
+        TextView item;
+    }
+}
--- AndroidStatus/src/main/java/info/curtbinder/reefangel/phone/NotificationsFragment.java
+++ AndroidStatus/src/main/java/info/curtbinder/reefangel/phone/NotificationsFragment.java
@@ -1,20 +1,201 @@
 package info.curtbinder.reefangel.phone;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class NotificationsFragment extends ListFragment {
+import info.curtbinder.reefangel.db.NotificationTable;
+import info.curtbinder.reefangel.db.StatusProvider;
+
+public class NotificationsFragment extends ListFragment
+        implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final String[] FROM = {
+            NotificationTable.COL_ID,
+            NotificationTable.COL_PARAM,
+            NotificationTable.COL_CONDITION,
+            NotificationTable.COL_VALUE};
+
+    private static RAApplication raApp;
+    private CheckBox ck;
+    private ListView lv;
+    private TextView tv;
+    private boolean fRunOnStartup;
 
     @Override
     public View onCreateView(
             LayoutInflater inflater,
             ViewGroup container,
             Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
         View root = inflater.inflate(R.layout.frag_notification, container, false);
+        raApp = (RAApplication) getActivity().getApplication();
+        ck = (CheckBox) root.findViewById(R.id.checkEnableNotification);
+        ck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(
+                    CompoundButton view,
+                    boolean isChecked) {
+                raApp.raprefs.set(R.string.prefNotificationEnableKey, isChecked);
+                enableDisableView(lv, isChecked);
+
+                // only toggle the empty list text if available
+                if (tv != null) {
+                    tv.setEnabled(isChecked);
+                }
+            }
+        });
+
+        lv = (ListView) root.findViewById(android.R.id.list);
+        tv = (TextView) root.findViewById(android.R.id.empty);
+        boolean fEnabledNotifications = raApp.raprefs.isNotificationEnabled();
+        ck.setChecked(fEnabledNotifications);
+        enableDisableView(lv, fEnabledNotifications);
+        // only toggle the empty list text if available
+        if (tv != null) {
+            tv.setEnabled(fEnabledNotifications);
+        }
+        fRunOnStartup = true;
         return root;
     }
 
+    public void enableDisableView(View view, boolean enabled) {
+        // todo hide action items from menu if disabled, show if enabled
+        view.setEnabled(enabled);
+
+        if (view instanceof ViewGroup) {
+            ViewGroup group = (ViewGroup) view;
+
+            for (int i = 0; i < group.getChildCount(); i++) {
+                enableDisableView(group.getChildAt(i), enabled);
+            }
+        }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.frag_notifications, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // if not enabled, don't allow changes to the list
+        if (!ck.isChecked()) {
+            return true;
+        }
+        switch (item.getItemId()) {
+            case R.id.action_delete_notification:
+                AlertDialog.Builder builder =
+                        new AlertDialog.Builder(getActivity());
+                builder.setMessage(getString(R.string.messageClearNotifications))
+                        .setCancelable(false)
+                        .setPositiveButton(getString(R.string.buttonYes),
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(
+                                            DialogInterface dialog,
+                                            int id) {
+                                        dialog.dismiss();
+                                        deleteAll();
+                                    }
+                                }
+                        )
+                        .setNegativeButton(getString(R.string.buttonNo),
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(
+                                            DialogInterface dialog,
+                                            int id) {
+                                        dialog.cancel();
+                                    }
+                                }
+                        );
+
+                AlertDialog alert = builder.create();
+                alert.show();
+                break;
+            case R.id.action_add_notification:
+//                Intent i = new Intent( getActivity(), NotificationPopupActivity.class );
+//                startActivity( i );
+                break;
+        }
+        return true;
+    }
+
+    private void deleteAll() {
+        Uri uri =
+                Uri.parse(StatusProvider.CONTENT_URI + "/"
+                        + StatusProvider.PATH_NOTIFICATION);
+        getActivity().getContentResolver().delete(uri, null, null);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+//        Intent i = new Intent( getActivity(), NotificationPopupActivity.class );
+//        Uri uri =
+//                Uri.parse( StatusProvider.CONTENT_URI + "/"
+//                        + StatusProvider.PATH_NOTIFICATION + "/" + id );
+//        i.putExtra( StatusProvider.NOTIFICATION_ID_MIME_TYPE, uri );
+//        startActivity( i );
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        Loader<Cursor> loader = null;
+        Uri content = Uri.parse(StatusProvider.CONTENT_URI + "/" + StatusProvider.PATH_NOTIFICATION);
+        if (id == 0) {
+            loader = new CursorLoader(getActivity(), content, FROM, null, null,
+                    NotificationTable.COL_ID + " ASC");
+        }
+        return loader;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        ListAdapter adapter = getListAdapter();
+        if (adapter == null || !(adapter instanceof CursorAdapter)) {
+            adapter = new NotificationListCursorAdapter(getActivity(), cursor, 0);
+            setListAdapter(adapter);
+        } else {
+            ((CursorAdapter) adapter).swapCursor(cursor);
+        }
+
+        if (fRunOnStartup) {
+            lv.post(new Runnable() {
+
+                @Override
+                public void run() {
+                    // to get the controls to update properly when the fragment
+                    // is loaded, we have to enable/disable the individual list
+                    // items after the loader finishes, otherwise we only have
+                    // the list disabled and the rest of the items "look"
+                    // enabled even though they are not
+                    enableDisableView(lv, raApp.raprefs.isNotificationEnabled());
+                    fRunOnStartup = false;
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> arg0) {
+        // on reset
+    }
 }
