835695ba36bc7c42c6f726b640f26714d72bd829
Curt Binder <curt.binder@gmail.com>
2014-08-06 14:57:57 -0400

    Removed unused import

--- src/info/curtbinder/reefangel/phone/StatusActivity.java
+++ src/info/curtbinder/reefangel/phone/StatusActivity.java
@@ -43,7 +43,6 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
