7ed3cfefb5f8f6844e29b587a4f9a2c850d71493
Curt Binder <curt.binder@gmail.com>
2014-08-09 18:29:20 -0400

    Added copyright to class

--- src/info/curtbinder/reefangel/service/XMLReadException.java
+++ src/info/curtbinder/reefangel/service/XMLReadException.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2011-2014 by Curt Binder (http://curtbinder.info)
+ * 
+ * This work is made available under the terms of the Creative Commons
+ * Attribution-NonCommercial-ShareAlike 3.0 Unported License
+ * http://creativecommons.org/licenses/by-nc-sa/3.0/
+ */
+
 package info.curtbinder.reefangel.service;
 
 public class XMLReadException extends Exception {
