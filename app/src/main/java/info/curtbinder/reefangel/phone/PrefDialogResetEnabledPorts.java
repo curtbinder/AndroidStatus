386a71acc5eb8d41659484ec333f40aac48c05f0
Curt Binder <curt.binder@gmail.com>
2015-03-01 18:09:35 -0600

    Added Reset Enabled Ports preference dialog

---
+++ app/src/main/java/info/curtbinder/reefangel/phone/PrefDialogResetEnabledPorts.java
@@ -0,0 +1,76 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Curt Binder
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package info.curtbinder.reefangel.phone;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Handler;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.Toast;
+
+import info.curtbinder.reefangel.controller.Controller;
+
+/**
+ * Created by binder on 3/1/15.
+ */
+public class PrefDialogResetEnabledPorts extends DialogPreference {
+
+    private static final String TAG = PrefDialogResetEnabledPorts.class.getSimpleName();
+
+    protected Context ctx;
+
+    public PrefDialogResetEnabledPorts(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.ctx = context;
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        super.onClick(dialog, which);
+
+        if (which == DialogInterface.BUTTON_POSITIVE) {
+            dialog.dismiss();
+            Handler h = new Handler();
+            final Runnable r = new Runnable() {
+                public void run() {
+                    resetEnabledPorts();
+                }
+            };
+            h.post(r);
+        }
+    }
+
+    private void resetEnabledPorts ( ) {
+        SettingsActivity settings = (SettingsActivity) ctx;
+        RAApplication raApp = (RAApplication) settings.getApplication();
+        raApp.raprefs.deleteRelayControlEnabledPorts();
+
+        Toast.makeText( ctx,
+                raApp.getString( R.string.messageResetEanbledPortsComplete ),
+                Toast.LENGTH_SHORT ).show();
+    }
+}
