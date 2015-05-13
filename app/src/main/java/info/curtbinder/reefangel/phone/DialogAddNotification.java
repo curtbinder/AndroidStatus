f61c482ec1a5d7d4d664d54a9b830cd8a3b20020
Curt Binder <curt.binder@gmail.com>
2015-05-13 18:05:34 -0500

    Improved Notifications dialogs
    Updated layout for Add/Delete notification. Switched to material theme.
    Cleaned up code.

--- app/src/main/java/info/curtbinder/reefangel/phone/DialogAddNotification.java
+++ app/src/main/java/info/curtbinder/reefangel/phone/DialogAddNotification.java
@@ -24,13 +24,18 @@
 
 package info.curtbinder.reefangel.phone;
 
+import android.app.Dialog;
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -43,17 +48,13 @@ import android.widget.Toast;
 import info.curtbinder.reefangel.db.NotificationTable;
 import info.curtbinder.reefangel.db.StatusProvider;
 
-public class DialogAddNotification extends DialogFragment
-    implements View.OnClickListener {
+public class DialogAddNotification extends DialogFragment {
 
     private static final String TAG = DialogAddNotification.class.getSimpleName();
 
     private Spinner paramSpinner;
     private Spinner condSpinner;
     private EditText valueText;
-    private Button saveButton;
-    private Button cancelButton;
-    private Button deleteButton;
     private long id;
     private Uri uri = null;
 
@@ -72,119 +73,114 @@ public class DialogAddNotification extends DialogFragment
         return d;
     }
 
+    @NonNull
     @Override
-    public View onCreateView(LayoutInflater inflater,
-                             ViewGroup container,
-                             Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.dlg_add_notification, container);
-        findViews(v);
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // TODO improve getting current theme
+        int themeId = R.style.AlertDialogStyle;
+        final ContextThemeWrapper themeWrapper = new ContextThemeWrapper(getActivity(), themeId);
+        LayoutInflater inflater = getActivity().getLayoutInflater().cloneInContext(themeWrapper);
+        AlertDialog.Builder builder = new AlertDialog.Builder(themeWrapper, themeId);
+        View root = inflater.inflate(R.layout.dlg_add_notification, null);
+        findViews(root);
         setAdapters();
         Bundle args = getArguments();
-        if ( args != null ) {
+        if (args != null) {
             uri = args.getParcelable(StatusProvider.NOTIFICATION_ID_MIME_TYPE);
             id = Long.parseLong(uri.getLastPathSegment());
             loadData();
         } else {
             id = -1;
         }
-        updateDisplay();
-        return v;
+        builder.setView(root);
+        builder.setPositiveButton(R.string.buttonSave, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                saveNotification();
+            }
+        });
+        if (isUpdate()) {
+            builder.setTitle(getString(R.string.titleUpdateNotification));
+            builder.setNegativeButton(R.string.buttonDelete, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    deleteNotification();
+                }
+            });
+            builder.setNeutralButton(R.string.buttonCancel, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dismiss();
+                }
+            });
+        } else {
+            builder.setTitle(getString(R.string.titleCreateNotification));
+            builder.setNegativeButton(R.string.buttonCancel, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dismiss();
+                }
+            });
+        }
+        return builder.create();
     }
 
     private void findViews(View v) {
         paramSpinner = (Spinner) v.findViewById(R.id.notifyParameterSpin);
         condSpinner = (Spinner) v.findViewById(R.id.notifyConditionSpin);
         valueText = (EditText) v.findViewById(R.id.notifyValue);
-        cancelButton = (Button) v.findViewById(R.id.cancelButton);
-        cancelButton.setOnClickListener(this);
-        deleteButton = (Button) v.findViewById(R.id.deleteButton);
-        deleteButton.setOnClickListener(this);
-        saveButton = (Button) v.findViewById(R.id.saveButton);
-        saveButton.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View v) {
-        switch ( v.getId() ) {
-            case R.id.deleteButton:
-                deleteNotification();
-                break;
-            case R.id.saveButton:
-                saveNotification();
-                break;
-        }
-        dismiss();
-    }
-
-    private void setAdapters ( ) {
+    private void setAdapters() {
         ArrayAdapter<CharSequence> p = ArrayAdapter.createFromResource(getActivity(),
-                        R.array.deviceParameters,
-                        android.R.layout.simple_spinner_item);
-        p.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item );
-        paramSpinner.setAdapter( p );
+                R.array.deviceParameters,
+                android.R.layout.simple_spinner_item);
+        p.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        paramSpinner.setAdapter(p);
         ArrayAdapter<CharSequence> c = ArrayAdapter.createFromResource(getActivity(),
-                        R.array.notifyConditions,
-                        android.R.layout.simple_spinner_item);
-        c.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item );
-        condSpinner.setAdapter( c );
+                R.array.notifyConditions,
+                android.R.layout.simple_spinner_item);
+        c.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        condSpinner.setAdapter(c);
     }
 
     private boolean isUpdate() {
-        return (id >-1);
+        return (id > -1);
     }
 
-    private void updateDisplay ( ) {
-        // updates the buttons at the bottom of the window
-        // based on the type of window we are displaying
-        // If we are updating the notification item, then we display
-        // Cancel, Delete, Save
-        // If we are creating a new notification item, then we display
-        // Cancel, Save
-
-        // Set the title of the window
-        // If updating, display Update Notification
-        // If new, display Create Notification
-        if ( isUpdate() ) {
-            getDialog().setTitle(getString(R.string.titleUpdateNotification));
-        } else {
-            deleteButton.setVisibility( View.GONE );
-            getDialog().setTitle(getString(R.string.titleCreateNotification));
-        }
-    }
-
-    private void loadData ( ) {
+    private void loadData() {
         String[] projection = {
                 NotificationTable.COL_PARAM,
                 NotificationTable.COL_CONDITION,
                 NotificationTable.COL_VALUE
         };
         Cursor c = getActivity().getContentResolver().query(uri, projection, null, null, null);
-        if ( c != null ) {
+        if (c != null) {
             c.moveToFirst();
 
-            if ( c.moveToFirst() ) {
+            if (c.moveToFirst()) {
                 paramSpinner.setSelection(c.getInt(
-                        c.getColumnIndex(NotificationTable.COL_PARAM) ), true );
+                        c.getColumnIndex(NotificationTable.COL_PARAM)), true);
                 condSpinner.setSelection(c.getInt(
                         c.getColumnIndex(NotificationTable.COL_CONDITION)), true);
-                valueText.setText( c.getString(
-                        c.getColumnIndex(NotificationTable.COL_VALUE) ) );
+                valueText.setText(c.getString(
+                        c.getColumnIndex(NotificationTable.COL_VALUE)));
             }
             c.close();
         }
     }
 
-    protected void saveNotification ( ) {
+    protected void saveNotification() {
         String s = valueText.getText().toString();
         Log.d(TAG, "Save Notification: " + paramSpinner.getSelectedItemPosition()
-                        + ", " + condSpinner.getSelectedItemPosition() + ", "
-                        + s);
+                + ", " + condSpinner.getSelectedItemPosition() + ", "
+                + s);
         // do error checking to ensure that there is a value
-        if ( TextUtils.isEmpty(s) ) {
+        if (TextUtils.isEmpty(s)) {
             // Empty string
-            Toast.makeText( getActivity(),
+            Toast.makeText(getActivity(),
                     getResources().getString(R.string.messageEmptyValue),
-                    Toast.LENGTH_SHORT ).show();
+                    Toast.LENGTH_SHORT).show();
             return;
         }
 
@@ -193,20 +189,20 @@ public class DialogAddNotification extends DialogFragment
         v.put(NotificationTable.COL_CONDITION, condSpinner.getSelectedItemPosition());
         v.put(NotificationTable.COL_VALUE, valueText.getText().toString());
 
-        if ( isUpdate() ) {
+        if (isUpdate()) {
             // update the values in the table
             getActivity().getContentResolver()
-                    .update( NotificationsFragment.NOTIFY_URI, v, NotificationTable.COL_ID + "=?",
-                            new String[] { Long.toString( id ) } );
+                    .update(NotificationsFragment.NOTIFY_URI, v, NotificationTable.COL_ID + "=?",
+                            new String[]{Long.toString(id)});
         } else {
             // Insert the values in the table
-            getActivity().getContentResolver().insert( NotificationsFragment.NOTIFY_URI, v );
+            getActivity().getContentResolver().insert(NotificationsFragment.NOTIFY_URI, v);
         }
     }
 
-    protected void deleteNotification ( ) {
-        Uri deleteUri = Uri.withAppendedPath( NotificationsFragment.NOTIFY_URI,
-                Long.toString( id ) );
-        getActivity().getContentResolver().delete( deleteUri, null, null );
+    protected void deleteNotification() {
+        Uri deleteUri = Uri.withAppendedPath(NotificationsFragment.NOTIFY_URI,
+                Long.toString(id));
+        getActivity().getContentResolver().delete(deleteUri, null, null);
     }
 }
