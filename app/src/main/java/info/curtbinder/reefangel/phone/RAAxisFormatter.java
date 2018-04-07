be5ec54bee4907a51fcdf5e9f36a503db235bca0
Curt Binder <curt.binder@gmail.com>
2018-04-06 21:20:44 -0500

    Added MarkerView class
    This is the start of adding in the MarkerView class which handles popup’s when you tap on the point on the chart.

---
+++ app/src/main/java/info/curtbinder/reefangel/phone/RAAxisFormatter.java
@@ -0,0 +1,10 @@
+package info.curtbinder.reefangel.phone;
+
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+
+/**
+ * Created by binder on 4/3/18.
+ */
+
+public class RAAxisFormatter implements IAxisValueFormatter {
+}
---
+++ app/src/main/java/info/curtbinder/reefangel/phone/RAMarkerView.java
@@ -0,0 +1,10 @@
+package info.curtbinder.reefangel.phone;
+
+import com.github.mikephil.charting.components.MarkerView;
+
+/**
+ * Created by binder on 4/2/18.
+ */
+
+public class RAMarkerView extends MarkerView {
+}
